da0c385764cc1a0bdc4be803a2f237fa865e09fd
n4n5 <its.just.n4n5@gmail.com>
2024-09-14 23:36:39 +0200

    add files

---
+++ README.md
@@ -0,0 +1,22 @@
+# maps history
+
+Visualize your google maps history day by day
+
+## How to use
+
+- export your google data (maps)
+- unzip the export
+- launch the script
+- enter path to journeys inside the export
+- the map will open
+- press enter to show the previous day
+
+## About
+
+In google maps export, there are 3 types of data:
+
+- places
+- journeys
+- segments
+
+Related to : <https://thoughts.n4n5.dev/google-maps-export/>
---
+++ main.py
@@ -0,0 +1,183 @@
+import pandas as pd
+import os
+import json
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+TIME_SPLIT = 10
+
+
+def extract_month(path_year, month):
+    places = {}
+    segments = {}
+
+    path_json = os.path.join(path_year, month)
+
+    with open(path_json) as f:
+        data = json.load(f)
+
+    data = data["timelineObjects"]
+    print("Number of data  : ", len(data), month)
+
+    for one_data in data:
+        if "activitySegment" not in one_data:
+            loc = one_data["placeVisit"]["location"]
+            duration = one_data["placeVisit"]["duration"]
+            text = ""
+            if "name" in loc:
+                text = loc["name"]
+            if "address" in loc:
+                text = loc["address"]
+
+            text += f"\n{duration['startTimestamp']} - {duration['endTimestamp']}"
+            time = duration["startTimestamp"][0:TIME_SPLIT]
+            if "latitudeE7" not in loc:
+                continue
+            seg = {
+                "color": "green",
+                "lat": loc["latitudeE7"] / 1e7,
+                "lon": loc["longitudeE7"] / 1e7,
+                "text": text,
+                "start": duration["startTimestamp"],
+                "end": duration["endTimestamp"],
+            }
+            if time in segments:
+                segments[time].append(seg)
+            else:
+                segments[time] = [seg]
+        else:
+            loc = one_data["activitySegment"]["startLocation"]
+            duration = one_data["activitySegment"]["duration"]
+            new_loc = {
+                "color": "red",
+                "lat": loc["latitudeE7"] / 1e7,
+                "lon": loc["longitudeE7"] / 1e7,
+                "start": duration["startTimestamp"],
+                "end": duration["endTimestamp"],
+                "text": f"{duration['startTimestamp']} - {duration['endTimestamp']}",
+            }
+            time = duration["startTimestamp"][0:TIME_SPLIT]
+            if time in places:
+                places[time].append(new_loc)
+            else:
+                places[time] = [new_loc]
+
+    return places, segments
+
+
+def extract_semantic(path_year):
+    if not os.path.exists(path_year):
+        print("File not found : ", path_year)
+        return [], []
+
+    places = {}
+    segments = {}
+    for file in os.listdir(path_year):
+        place, segment = extract_month(path_year, file)
+        places.update(place)
+        segments.update(segment)
+
+    return places, segments
+
+
+path = input("enter absolute path to the google takeout/journeys\n")
+
+segments = {}
+places = {}
+
+sementic = os.path.join(path, "Semantic Location History")
+
+for year in os.listdir(sementic):
+    path_year = os.path.join(sementic, year)
+    if os.path.isdir(path_year):
+        print("Year : ", year)
+        place, segment = extract_semantic(path_year)
+        print("Number of days for places : ", len(place))
+        print("Number of fays for segments : ", len(segment))
+        segments.update(segment)
+        places.update(place)
+
+
+path_records = os.path.join(path, "Records.json")
+with open(path_records, "r") as f:
+    records = json.load(f)
+
+
+data_records = {}
+for record in records["locations"]:
+    spurce = ""
+    if "source" in record:
+        source = record["source"]
+    else:
+        source = record["formFactor"]
+    text = ""
+    if "address" in record:
+        text = record["address"]
+    if "latitudeE7" not in record:
+        continue
+    seg = {
+        "color": "blue",
+        "lat": record["latitudeE7"] / 1e7,
+        "lon": record["longitudeE7"] / 1e7,
+        "text": text,
+        "timestamp": record["timestamp"],
+    }
+
+    time = record["timestamp"][0:TIME_SPLIT]
+    if time in data_records:
+        data_records[time].append(seg)
+
+    else:
+        data_records[time] = [seg]
+
+
+def show_date(choosen_date):
+    if choosen_date in segments:
+        df = pd.DataFrame(segments[choosen_date])
+        fig = px.line_map(df, lat="lat", lon="lon", color="color", text="text")
+    else:
+        fig = go.Figure()
+
+    fig.update_layout(title=choosen_date, map_zoom=3)
+    if choosen_date in data_records:
+        fdd = pd.DataFrame(data_records[choosen_date])
+        fig.add_trace(
+            go.Scattermap(
+                mode="markers+lines",
+                lon=fdd["lon"],
+                lat=fdd["lat"],
+                marker={"size": 10},
+            )
+        )
+
+    if choosen_date in places:
+        df = pd.DataFrame(places[choosen_date])
+        fig.add_trace(
+            go.Scattermap(
+                mode="markers",
+                lon=df["lon"],
+                lat=df["lat"],
+                marker={"size": 10},
+            )
+        )
+
+    fig.show()
+
+
+# sort by date
+segments = dict(sorted(segments.items(), key=lambda item: item[0]))
+places = dict(sorted(places.items(), key=lambda item: item[0]))
+data_records = dict(sorted(data_records.items(), key=lambda item: item[0]))
+
+# get all dates
+all_dates = sorted(
+    set(list(segments.keys()) + list(places.keys()) + list(data_records.keys()))
+)
+
+all_dates.reverse()
+
+for key in all_dates:
+    print("Date : ", key)
+    show_date(key)
+    input("Press Enter to continue...\n")
